bf6b0a458fd1c4b872e927616128172d47a35e4d
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-09-15 00:32:13 -0300

    Menu no console e printar lados

--- src/com/company/Arvore.java
+++ src/com/company/Arvore.java
@@ -12,8 +12,13 @@ public class Arvore<T extends Comparable> {
     public void adicionar(T valor)
     {
         Elemento<T> novoElemento = new Elemento<>(valor);
+        Integer branch = 1;
 
-        if(this.raiz == null) this.raiz = novoElemento;
+        if(this.raiz == null)
+        {
+            this.raiz = novoElemento;
+            System.out.println("Elemento adicionado na raiz");
+        }
         else
         {
             Elemento<T> atual = this.raiz;
@@ -21,22 +26,85 @@ public class Arvore<T extends Comparable> {
             {
                 if(novoElemento.getValor().compareTo(atual.getValor()) == -1)
                 {
-                    if(atual.getEsquerda() != null) atual = atual.getEsquerda();
+                    if(atual.getEsquerda() != null)
+                    {
+                        atual = atual.getEsquerda();
+                        branch++;
+                    }
                     else
                     {
                         atual.setEsquerda(novoElemento);
+                        System.out.println("Elemento adicionado na "+branch+"º branch a esquerda");
                         break;
                     }
                 }
                 else
                 {
-                    if(atual.getDireita() != null) atual = atual.getDireita();
+                    if(atual.getDireita() != null)
+                    {
+                        atual = atual.getDireita();
+                        branch++;
+                    }
                     else
                     {
                         atual.setDireita(novoElemento);
+                        System.out.println("Elemento adicionado na "+branch+"º branch a direita");
                         break;
                     }
                 }
+            }
+        }
+    }
+
+    public void printarEsquerda()
+    {
+        if(this.raiz == null) System.out.println("Árvore atualmente não possui nenhum elemento");
+        else
+        {
+            System.out.println("Valor da raiz: "+this.raiz.getValor());
+            Elemento<T> elementoAtual = this.raiz;
+            Integer branch = 1;
+
+            if(elementoAtual.getEsquerda() != null) {
+                System.out.println("Valor a esquerda : " + elementoAtual.getEsquerda());
+                elementoAtual = elementoAtual.getEsquerda();
+            }
+
+            while(true)
+            {
+                if(elementoAtual.getEsquerda() != null && elementoAtual != this.raiz)
+                {
+                    System.out.println("Elemento da "+branch+"º branch a esquerda: "+elementoAtual.getEsquerda() +" e a direita: "+elementoAtual.getEsquerda());
+                    elementoAtual = elementoAtual.getEsquerda();
+                }
+                else break;
+            }
+        }
+    }
+
+    public void printarDireita()
+    {
+        if(this.raiz == null) System.out.println("Árvore atualmente não possui nenhum elemento");
+        else
+        {
+            System.out.println("Valor da raiz: "+this.raiz.getValor());
+            Elemento<T> elementoAtual = this.raiz;
+            Integer branch = 1;
+
+            if(elementoAtual.getDireita() != null)
+            {
+                System.out.println("Valor a esquerda : " + elementoAtual.getDireita().toString());
+                elementoAtual = elementoAtual.getDireita();
+            }
+
+            while(true)
+            {
+                if(elementoAtual.getDireita() != null && elementoAtual != this.raiz)
+                {
+                    System.out.println("Elemento da "+branch+"º branch a esquerda: "+elementoAtual.getEsquerda().toString() +" e a direita: "+elementoAtual.getEsquerda().toString());
+                    elementoAtual = elementoAtual.getEsquerda();
+                }
+                else break;
             }
         }
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Arvore<Integer> arvore = new Arvore<>();
+        Scanner s = new Scanner(System.in);
 
+        Integer menu = 1;
+        while (menu != 0)
+        {
+            System.out.println("\n\nDigite 1 para adicionar mais um valor. \n" +
+                    "Digite 2 para imprimir os valores a esquerda.\n" +
+                    "Digite 3 para imprimir os valores a direita.\n" +
+                    "Digite qualquer outro número para sair.\n");
+            System.out.print("Escolha:");
+            menu = s.nextInt();
+
+            if(menu == 1)
+            {
+                System.out.print("Digite o valor que deseja adicionar: ");
+                Integer valor = s.nextInt();
+                arvore.adicionar(valor);
+            }
+            else if(menu == 2) arvore.printarEsquerda();
+            else if(menu == 3) arvore.printarDireita();
+            else break;
+        }
     }
 }
