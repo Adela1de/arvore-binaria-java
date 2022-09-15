2b6318dd0e938654f4e64988cd0174a3aa873f00
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-09-14 23:35:16 -0300

    Base do algoritmo pronto

---
+++ src/com/company/Elemento.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Elemento<T> {
+
+    private T valor;
+    private Elemento<T> esquerda;
+    private Elemento<T> direita;
+
+    public Elemento(T valor)
+    {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+
+    public T getValor() {
+        return valor;
+    }
+
+    public Elemento<T> getEsquerda() {
+        return esquerda;
+    }
+
+    public Elemento<T> getDireita() {
+        return direita;
+    }
+
+    public void setEsquerda(Elemento<T> esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public void setDireita(Elemento<T> direita) {
+        this.direita = direita;
+    }
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+}
